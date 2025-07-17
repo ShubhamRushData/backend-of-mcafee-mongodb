f5cb1fa7cc2f37dfa88d39f29dd3099e24a4046c
TechDev0001 <shubhamyadav203920@gmail.com>
2025-07-17 16:12:14 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api/blogs', blogRoutes);
+app.use('/api/posts', blogRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
